268f093fb01e37565e3fc95ce2ca13de9a0ac1a6
Olena Hladkovska <ohladkov@student.42berlin.de>
2024-09-07 22:04:21 +0200

    working on mariadb configuration

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-service mysql start;
+mysql
 . /run/secrets/db_password;
 . /run/secrets/db_root_password;
-sleep 10
+# sleep 10
 mysql -e "CREATE DATABASE IF NOT EXISTS $db_name;"
 mysql -e "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_password';"
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$db_user'@'%' WITH GRANT OPTION;"
-mysql -e "ALTER USER 'root'@'%' IDENTIFIED BY $db_root_password"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY $db_root_password;"
 mysql -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p$db_root_password shutdown
-exec mysqld
+# exec mysqld
+exec /usr/bin/mysqld_safe
--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -7,7 +7,10 @@ sleep 15
 . /run/secrets/db_password
 . /run/secrets/db_root_password
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname="$db_name" --dbuser="$db_user" --dbpass="$db_password" --dbhost="$db_host" --path="/var/www/html" --allow-root
+echo "./wp-cli.phar core download --allow-root"
+./wp-cli.phar config create --dbname="$db_name" --dbuser="$db_user" --dbpass="$db_password" --dbhost="$db_host":3306 --path="/var/www/html" --allow-root
+echo "./wp-cli.phar config create --dbname="$db_name" --dbuser="$db_user" --dbpass="$db_password" --dbhost="$db_host":3306 --path="/var/www/html" --allow-root"
 ./wp-cli.phar core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$db_root_password" --admin_email="$WP_ADMIN_EMAIL" --allow-root
+echo "./wp-cli.phar core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$db_root_password" --admin_email="$WP_ADMIN_EMAIL" --allow-root"
 echo "\t --- FINISHED ---- ./wp-cli.phar"
 /usr/sbin/php-fpm7.3 -F
